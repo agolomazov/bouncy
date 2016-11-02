58efd82dc2b2559f78ed6766f7464083864601cc
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2016-11-02 21:22:08 +0300

    Добалены стили для блока subscribe

--- markup/components/subscribe/subscribe.js
+++ markup/components/subscribe/subscribe.js
@@ -1,1 +1,20 @@
-
+$(document).ready(function () {
+    var $windowSize = $(this).outerWidth();
+    if ($windowSize <= 600) {
+        $('.form-subscribe').equalize({
+            target: '.form-element',
+            property: 'height'
+        });
+    }
+    $( window ).on('resize', function ( e ) {
+        var windowSize = $(document).outerWidth();
+        if (windowSize <= 600) {
+            $('.form-subscribe').equalize({
+                target: '.form-element',
+                property: 'height'
+            });
+        } else {
+            $('.form-subscribe').equalize('destroy');
+        }
+    });
+});
