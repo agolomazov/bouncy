ae8bae2f4054f6ff25394b8272557bdb7a688713
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2016-11-03 18:34:49 +0300

    Добавлен JS для слайдера отзывов в блоке testimonials

--- markup/components/testimonials/testimonials.js
+++ markup/components/testimonials/testimonials.js
@@ -1,1 +1,31 @@
-
+$(document).ready(function () {
+    var $sliderSlick = $('.testimonials__list');
+    var $destroyed = true;
+    var $documentSize = $(document).innerWidth();
+    var $sliderOptions = {
+        arrows: false,
+        dots: true,
+        dotsClass: 'testimonials__dots',
+        infinite: true
+    };
+    if ($documentSize > 600) {
+        $sliderSlick.slick($sliderOptions);
+        $sliderSlick.slick('init', function () {
+            $(this).equalize({
+                target: '.testimonials__list-item',
+                property: 'height'
+            });
+        });
+        $destroyed = !$destroyed;
+    }
+    $(window).on('resize', function (e) {
+        var documentSize = $(document).innerWidth();
+        if (documentSize < 600 && !$destroyed) {
+            $sliderSlick.slick('unslick');
+            $destroyed = !$destroyed;
+        } else if (documentSize >= 600 && $destroyed) {
+            $sliderSlick.slick($sliderOptions);
+            $destroyed = !$destroyed;
+        }
+    });
+});
