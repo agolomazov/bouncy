bba74b6af8ab151ef37678d4381c30923df47d56
Majesko <majesko@gmail.com>
2016-11-03 19:54:39 +0300

    wip: работаю над прогрессбаром

--- markup/components/team/team.js
+++ markup/components/team/team.js
@@ -1,1 +1,16 @@
-
+var progressBar = (function () {
+    var init = function (selector) {
+        let val = $(selector).data('value');
+        let bar = $(selector).find('.team-member__stats-line');
+        $(bar).animate({
+            width: val,
+        }, 500);
+    };
+    return {
+        init
+    };
+})();
+// progressBar.init();
+$('.team-member__stats').each(function () {
+    progressBar.init(this);
+});
