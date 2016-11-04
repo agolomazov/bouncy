270a500508f67b822aa85aff531b423c68eeabde
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2016-11-04 14:40:47 +0300

    Добавлен адаптив для меню в шапке

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -1,1 +1,13 @@
+/* eslint-disable */
+$('.nav-toggle').click(function (e) {
+    $('.nav-menu').slideToggle(500);
+});
 
+debounce(function(){
+    $(window).resize(function(){
+        if($(window).width() > 720){
+            $('.nav-menu').removeAttr('style');
+        }
+    });
+}, 200);
+/* eslint-enable */
