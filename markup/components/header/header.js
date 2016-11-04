b30815c324d90e99bb3fca25eb6d00b5cdfef4e8
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2016-11-04 14:51:01 +0300

    FIX - поправлен JS для адаптива меню

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -3,11 +3,9 @@ $('.nav-toggle').click(function (e) {
     $('.nav-menu').slideToggle(500);
 });
 
-debounce(function(){
-    $(window).resize(function(){
-        if($(window).width() > 720){
-            $('.nav-menu').removeAttr('style');
-        }
-    });
-}, 200);
+$(window).resize(function(){
+    if($(window).width() > 720){
+        $('.nav-menu').removeAttr('style');
+    }
+});
 /* eslint-enable */
