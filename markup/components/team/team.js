8da05408455d8f3738634d3341278a485c697810
Majesko <majesko@gmail.com>
2016-11-04 12:57:53 +0300

    upd: закончил работы над responsive

--- markup/components/team/team.js
+++ markup/components/team/team.js
@@ -9,7 +9,41 @@ var progressBar = (function () {
         }
     };
 })();
-// progressBar.init();
-$('.team-member__stats').each(function () {
-    progressBar.init(this, 3000);
+
+$(document).ready(function () {
+    $('.team-member__stats').each(function () {
+        progressBar.init(this, 3000);
+    });
+});
+
+
+$(document).ready(function () {
+    let slider = $('.team-slider__list');
+    let options = {
+        dots: true,
+        dotsClass: 'team-slider__dots',
+        infinite: true
+    };
+    let documentSize = $(document).innerWidth();
+    let destroyed = true;
+    if (documentSize > 650) {
+        slider.slick(options);
+        slider.slick('init', function () {
+            $(this).equalize({
+                target: '.team-slider__item',
+                property: 'height'
+            });
+        });
+        destroyed = !destroyed;
+    }
+    $(window).on('resize', function () {
+        let docSize = $(document).innerWidth();
+        if (docSize < 650 && !destroyed) {
+            slider.slick('unslick');
+            destroyed = !destroyed;
+        } else if (docSize >= 650 && destroyed) {
+            slider.slick(options);
+            destroyed = !destroyed;
+        }
+    });
 });
