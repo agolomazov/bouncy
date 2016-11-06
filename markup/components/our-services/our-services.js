9dacc75c847568743076dd8a0e7bece414c8a8f2
Majesko <majesko@gmail.com>
2016-11-06 12:31:26 +0300

    upd: responsive и плагины
    Вроде готово.

--- markup/components/our-services/our-services.js
+++ markup/components/our-services/our-services.js
@@ -1,35 +1,116 @@
-var servicesTabs = (() => {
-    const params = {
+var tabsController = (() => {
+    let props = {
         container: '',
-        tabClass: ''
+        height: 0
     };
 
-    let hideAll = (selector) => {
-        $(selector).hide();
+    let calculateHeight = () => {
+        props.container.find('.tab').each(function () {
+            $(this).css('display', 'block');
+            console.log($(this).height());
+            if (props.height < props.container.height()) {
+                props.height = props.container.height();
+            }
+            $(this).css('display', 'none');
+        });
+        props.container.find('.tab').each(function () {
+            $(this).css('display', '');
+        });
+        console.log(props.height);
+        return props.height;
     };
 
-    let switchTab = (selector) => {
-        hideAll(params.tabClass);
-        $('#' + selector).show();
-    };
-
-    let init = (selector, options) => {
-        params.container = $(selector);
-        params.tabClass = options.tabClass;
-        params.activeClass = options.activeClass;
-        $('.our-services__tabs-link').on('click', function (e) {
-            e.preventDefault();
-            switchTab($(this).data('target'));
+    let switchTabs = (target) => {
+        let el = props.container.find('.tab-active');
+        // el.css('opacity', '0');
+        el.animate({
+            opacity: 0
+        }, 200, function () {
+            el.removeClass('tab-active');
+            target.addClass('tab-active');
+            target.animate({
+                opacity: 1
+            }, 200);
         });
     };
 
+    let clearActiveLinks = () => {
+        props.container.find('.link-active').removeClass('link-active');
+    };
 
+    let processClick = function (e) {
+        let el = $(this);
+        let targetId = el.data('target');
+        let target = $('#' + targetId);
 
+        e.preventDefault();
+        clearActiveLinks();
 
+        el.addClass('link-active');
+        switchTabs(target);
+    };
+
+    let init = (selector) => {
+        props.container = $(selector);
+        // props.container.css('min-height', calculateHeight() + 'px');
+        $('.tab').each(function () {
+            $(this).css('opacity', '0');
+        });
+        $('.tab-active').css('opacity', '1');
+        $('.tab-link').on('click', processClick);
+    };
 
     return {
         init
     };
 })();
 
-servicesTabs.init('services-tabs', {tabClass: '.our-services__service', activeClass: 'service-active'});
+tabsController.init('#services-tabs');
+
+
+$('#service1').roundSlider({
+    radius: 60,
+    width: 4,
+    handleSize: '+16',
+    handleShape: 'dot',
+    sliderType: 'min-range',
+    value: 85,
+    readOnly: true,
+    startAngle: 0,
+    tooltipFormat: 'skillVal1'
+});
+function skillVal1(args) {
+    return args.value;
+}
+
+$('#service2').roundSlider({
+    radius: 60,
+    width: 4,
+    handleSize: '+16',
+    handleShape: 'dot',
+    sliderType: 'min-range',
+    value: 75,
+    readOnly: true,
+    startAngle: 0,
+    tooltipFormat: 'skillVal2'
+});
+function skillVal2(args) {
+    return args.value;
+}
+
+$('#service3').roundSlider({
+    radius: 60,
+    width: 4,
+    handleSize: '+16',
+    handleShape: 'dot',
+    sliderType: 'min-range',
+    value: 85,
+    readOnly: true,
+    startAngle: 0,
+    tooltipFormat: 'skillVal3'
+});
+function skillVal3(args) {
+    return args.value;
+}
+
+$('.rs-tooltip-text').counterUp();
